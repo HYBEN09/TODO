443800fe71f366199b4d77a1bb373b967db24802
hyebeen byun <hyebeen2658@naver.com>
2023-09-22 01:01:52 +0900

    style: Todo UI 임시 구현

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,5 +1,26 @@
 function Todo() {
-  return <div className="min-h-custom bg-sky-100 dark:bg-gray-800 ">Todo</div>;
+  return (
+    <div className="min-h-custom bg-sky-100 dark:bg-gray-800 flex justify-center items-center">
+      <div className="container-box">
+        <h2 className="title">TODO LIST</h2>
+
+        <ul>
+          <li>
+            <label>
+              <input type="checkbox" />
+              <span>TODO 1</span>
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              <span>TODO 2</span>
+            </label>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
