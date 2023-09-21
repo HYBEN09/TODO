fd5be88d1146c32894a5415280e4df4b23959557
hyebeen byun <hyebeen2658@naver.com>
2023-09-21 11:04:26 +0900

    Feat: 페이지 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
-function App() {
-  return <div className="text-lg">app</div>;
-}
+import { Suspense, lazy } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+/* PAGES -------------------------------------------------------------------- */
+const NotFound = lazy(() => import('./pages/NotFound'));
+const Layout = lazy(() => import('./pages/Layout'));
+const Home = lazy(() => import('./pages/Home'));
+const SignIn = lazy(() => import('./pages/Signin'));
+const SignUp = lazy(() => import('./pages/Signup'));
+const Todo = lazy(() => import('./pages/Todo'));
+
+/* ROUTES ------------------------------------------------------------------- */
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: '/',
+        element: <>main</>,
+      },
+      {
+        path: '/signin',
+        element: <SignIn />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/todo',
+        element: <Todo />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+}
 export default App;
