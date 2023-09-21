a46c9af71b08076d37311b781253e8d0b2df8cdf
hyebeen byun <hyebeen2658@naver.com>
2023-09-21 22:19:04 +0900

    feat: 사용자 인증 관련 기능을 구현 - AuthService 클래스를 정의

---
+++ src/services/AuthService.js
@@ -0,0 +1,40 @@
+//* AuthService interface
+
+//* signin (email, password):Promise<undefined>
+//* signup (email, password):Promise<undefined>
+//* logout ():undefined
+
+export class AuthServiceImpl {
+  constructor(httpClient, storage) {
+    this.httpClient = httpClient;
+    this.storage = storage;
+  }
+
+  async signin(email, password) {
+    const response = await this.httpClient.fetch("auth/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const { access_token } = await response.json();
+
+    this.storage.save(access_token);
+  }
+
+  async signup(email, password) {
+    this.httpClient.fetch("auth/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+  }
+
+  logout() {
+    this.storage.delete();
+  }
+}
