3e501307e85b7919cccf63110ac884751543aa55
hyebeen byun <hyebeen2658@naver.com>
2023-09-24 10:07:24 +0900

    style: Home 페이지 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 function Home() {
-  return <div className="min-h-custom bg-sky-100 dark:bg-gray-800 ">Home</div>;
+  return (
+    <div className="min-h-custom bg-sky-100 dark:bg-gray-800 flex justify-center items-center">
+      <img src="/assets/pre-oneboarding.png" alt="" width={800} />
+    </div>
+  );
 }
 
 export default Home;
